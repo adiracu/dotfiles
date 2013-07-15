265e75f60e04f0c006bbaf8ca72bf43791f9f462
Adi @ MBPr <adrianracu@Adrians-MBPr-3.local>
2013-07-15 23:09:59 +0300

    Reformatted

--- source/command_tips.sh
+++ source/command_tips.sh
@@ -30,15 +30,7 @@ echo -e "`emph_it man 1-9 page_name`              : gets the page from that sect
 "'
 
 alias lstips='
-echo -e "`emph_it ls -h`              : print in human readable format"
-`emph_it ls -S`              : sort by sizerm
-'
-# alias mantips='echo "aaa" `emph_it lalala` bbbb'
-# `whatis $word`	: searches in the NAMES of man pages
-# 					: useful pentru ca iti listeaza manualele
-# `apropos $word`: search in the NAMES of man pages AND the very short description (manual synopsis)
-# 					: both can use regex, e.g. `whatis -w *name`
-# 					: foloseste `whatis` daca nu iti mai amintesti exact cum se numeste o comanda
-# 					: e.g. `whatis -w *word`
-					# : foloseste `apropos` daca vrei sa cauti o comanda dupa ce face
+echo -e "`emph_it ls -h`              : print in human readable format
+`emph_it ls -S`              : sort by size
+"'
 
