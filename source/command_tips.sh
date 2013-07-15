9d374e543d7e3396cfd4fbd2263514272f998c56
Adi @ MBPr <adrianracu@Adrians-MBPr-3.local>
2013-07-15 23:07:10 +0300

    Updated mantips and adde lstips

--- source/command_tips.sh
+++ source/command_tips.sh
@@ -6,6 +6,7 @@ function emph_it()   { echo -e "\033[0;36m$@\033[0m"; }
 
 alias mantips='
 echo -e "`emph_it man 1-9 page_name`              : gets the page from that section
+
 `emph_it man -f word_to_search_for`      : search in the NAMES of man pages
                                : useful if you can'\''t remember the exact name of a command
                                : same as `emph_it whatis word_to_search_for`
@@ -19,12 +20,19 @@ echo -e "`emph_it man 1-9 page_name`              : gets the page from that sect
                                : brute force
                                : it asks if it should open after each page it finds
 
-`emph_it man -s 3 -K word_to_search_for` : specify sections to narow search
-`emph_it man -s X -K --regex REGEX`
+`emph_it man -s N -K word_to_search_for` : specify sections to narow search
+
+`emph_it man -s N -K --regex REGEX`      : use a regex
+
+`emph_it manjump word_to_jump_to page`   : same as opening the page and searching for word
 
 `emph_it zcat`                           : cat for man pages
 "'
 
+alias lstips='
+echo -e "`emph_it ls -h`              : print in human readable format"
+`emph_it ls -S`              : sort by sizerm
+'
 # alias mantips='echo "aaa" `emph_it lalala` bbbb'
 # `whatis $word`	: searches in the NAMES of man pages
 # 					: useful pentru ca iti listeaza manualele
