196c0ece10964c9bee8427f2a45eb89b17d64bad
Adi @ MBPr <Adi @ MBPr>
2014-03-08 12:41:08 +0200

    zsh instructions

--- conf/firsttime_reminder.sh
+++ conf/firsttime_reminder.sh
@@ -8,6 +8,9 @@ source ~/.bashrc
 
 cat <<EOF
 Install useful commands/packages :  ~/.dotfiles/bin/install_packages_for_labels
-Change default shell used : chsh -s /bin/zsh
+Change default shell used :
+	brew install by default in /usr/local/bin so:
+		1. add /usr/local/bin to /etc/shells
+		2. chsh -s /usr/local/bin/zsh
 SSH password-less stuff : sshtips
 EOF
